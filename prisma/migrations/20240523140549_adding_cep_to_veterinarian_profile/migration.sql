bdee27ad1d85f6ff3f0ef9bf3e71716a9535e8ee
luizjoliver <luizfellipejoliver14@gmail.com>
2024-08-28 20:50:29 -0300

    Backend Appointments CRUD - Create , get all , get by id , delete , update by id

--- prisma/migrations/20240523140549_adding_cep_to_veterinarian_profile/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `cep` to the `VeterinarianProfile` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "VeterinarianProfile" ADD COLUMN     "cep" TEXT NOT NULL;
