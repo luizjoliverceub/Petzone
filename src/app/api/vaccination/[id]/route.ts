be5bc4a2dbc47ff4461827550bc7bcc90f3f0654
luizjoliver <luizfellipejoliver14@gmail.com>
2024-09-26 20:48:35 -0300

    Refatoração rotas vaccination , criação rota buscar todas vacinas beaseado no id do pet , buscar vacina pelo id e deletar vacina pelo id

--- src/app/api/vaccination/[id]/route.ts
+++ src/app/api/vaccination/[id]/route.ts
@@ -1,44 +1,7 @@
 import { prisma } from "@/utils/db/prisma"
 import { NextResponse } from "next/server"
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
 
-    const session = request.headers.get("session")
-    const newSessionValue = JSON.parse(session)
-   
-     
-     
-       // if(session && newSessionValue ){
-        const petId = params.id
-
-      try {
-         
-       
-       const allVaccinations = await prisma.vaccination.findMany({
-        where:{
-          petId:petId
-        }
-       })
-   
-       if(allVaccinations.length === 0) return new NextResponse(JSON.stringify({message:"Nenhuma vácina cadastrada para este pet ainda :)"}),{status:200})
-
-
-        return new NextResponse(JSON.stringify(allVaccinations),{status:200})
-   
-      } catch (error) {
-       console.log(error);
-         return new NextResponse(
-           JSON.stringify({ message: "Something went wrong!"}),
-           { status: 500 }
-         );
-      }
-   // }else{
-      
-   //     return new NextResponse(JSON.stringify({message:"you are not authenticated"}),
-   //     {status:401})
-   // }
-       
-     }
    
 
      export async function DELETE(request: Request, { params }: { params: { id: string } }) {
@@ -82,4 +45,85 @@ export async function GET(request: Request, { params }: { params: { id: string }
        
          
    
+
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+
+    const session = request.headers.get("session")
+    const newSessionValue = JSON.parse(session)
+   
+     
+     
+       // if(session && newSessionValue ){
+        const vaccinationId = params.id
+
+      try {
+         
+       
+       const allVaccinations = await prisma.vaccination.findUnique({
+        where:{
+          id:vaccinationId
+        }
+       })
+   
+       if(!allVaccinations) return new NextResponse(JSON.stringify({message:"Esta vácina não foi encontrada ou não existe!"}),{status:200})
+
+
+        return new NextResponse(JSON.stringify(allVaccinations),{status:200})
+   
+      } catch (error) {
+       console.log(error);
+         return new NextResponse(
+           JSON.stringify({ message: "Something went wrong!"}),
+           { status: 500 }
+         );
+      }
+   // }else{
+      
+   //     return new NextResponse(JSON.stringify({message:"you are not authenticated"}),
+   //     {status:401})
+   // }
+       
+     }
+
+
+    //  export async function PUT(request: Request, { params }: { params: { id: string } }) {
+
+    //   const session = request.headers.get("session")
+    //   const newSessionValue = JSON.parse(session)
+     
+       
+       
+    //      // if(session && newSessionValue ){
+    //       const vaccinationId = params.id
+  
+    //     try {
+           
+         
+    //      const allVaccinations = await prisma.vaccination.delete({
+    //       where:{
+    //           id:vaccinationId
+    //       }
+    //      })
+     
+  
+  
+    //       return new NextResponse(JSON.stringify({message:"Vacina deletada com sucesso!"}),{status:200})
+     
+    //     } catch (error) {
+    //      console.log(error);
+    //        return new NextResponse(
+    //          JSON.stringify({ message: "Something went wrong!"}),
+    //          { status: 500 }
+    //        );
+    //     }
+    //  // }else{
+        
+    //  //     return new NextResponse(JSON.stringify({message:"you are not authenticated"}),
+    //  //     {status:401})
+    //  // }
+         
+    //    }
+     
+
      
---
+++ src/app/api/vaccination/pet/[id]/route.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/utils/db/prisma"
+import { NextResponse } from "next/server"
+
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+
+  const session = request.headers.get("session")
+  const newSessionValue = JSON.parse(session)
+ 
+   
+   
+     // if(session && newSessionValue ){
+      const petId = params.id
+
+    try {
+       
+     
+     const allVaccinations = await prisma.vaccination.findMany({
+      where:{
+        petId:petId
+      }
+     })
+ 
+     if(allVaccinations.length === 0) return new NextResponse(JSON.stringify({message:"Nenhuma vácina cadastrada para este pet ainda :)"}),{status:200})
+
+
+      return new NextResponse(JSON.stringify(allVaccinations),{status:200})
+ 
+    } catch (error) {
+     console.log(error);
+       return new NextResponse(
+         JSON.stringify({ message: "Something went wrong!"}),
+         { status: 500 }
+       );
+    }
+ // }else{
+    
+ //     return new NextResponse(JSON.stringify({message:"you are not authenticated"}),
+ //     {status:401})
+ // }
+     
+   }
